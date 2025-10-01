af38a1e5d54b71b4d9ed55a3527e96a464fc7052
nightcrawler1256 <naveenkumar3112002@gmail.com>
2025-10-01 15:12:02 +0530

    Employee Manager File

---
+++ src/EmployeeManager.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class EmployeeManager {
+
+    private Map<Integer,Employee> employeeData;
+
+    public EmployeeManager()
+    {
+        this.employeeData=new HashMap<>();
+    }
+
+    public void addEmployee(Employee employee)
+    {
+        employeeData.put(employee.getId(),employee);
+    }
+
+    public List<Employee> getAllEmployee(){
+        return new ArrayList<>(employeeData.values());
+    }
+
+    public List<Employee> filterEmployeesBySalary(Double minSalary)
+    {
+        return employeeData.values().stream()
+        .filter(employee -> employee.getSalary().orElse(0.0)>=minSalary)
+        .collect(Collectors.toList());
+    }
+
+    public List<Employee> sortEmployeeBySalary()
+    {
+        return employeeData.values().stream()
+        .sorted(Comparator.comparingDouble(e->e.getSalary().orElse(0.0)))
+               .collect(Collectors.toList());
+    }
+    
+}
